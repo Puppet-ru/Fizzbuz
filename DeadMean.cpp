d21b0d365f474a33c03405fcf82d694d262b2528
Puppet-ru <73840538+Puppet-ru@users.noreply.github.com>
2021-09-20 13:36:59 +0300

    Add files via upload

---
+++ DeadMean.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for (i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+            printf("Fizz");
+        }
+        if (i % 5 == 0) {
+            printf("Buzz");
+        }
+        if (i % 5 != 0 && i % 3 != 0)
+            printf("%d", i);
+        printf("\n");
+    }
+    return 0;
+}
+
